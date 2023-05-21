cd4ce278d56a4f0d63f7f9c3a3dc151516a4120d
Johannes Esbjörnsson <johannesesbjornsson@Johanness-MBP.fritz.box>
2023-05-21 13:10:08 +0100

    starting up

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # crypto-tax-estimate
 How much do I owe the government ish
+
+## Generate CSV
+
+### Binance
+
+log into binance.com
+
+Go to wallet > Transaction History > Export Transaction Records
+
+
+
+## Generate API key
+
+### Binance 
+
+Go to Api Management
+
+Create Api Key
---
+++ main.py
@@ -0,0 +1,35 @@
+
+import configparser
+from binance.client import Client
+import csv
+import os
+
+
+def get_binance_transactions(file):
+    earn_titels = ["Simple Earn Locked Rewards", "Simple Earn Flexible Interest", "ETH 2.0 Staking Rewards"]
+    tokens = {}
+    with open(file) as csv_file:
+        rows = csv.reader(csv_file, delimiter=',')
+        for line in rows:
+            trade_id, trade_date, trade_type, title, token, amount, description = line
+            if title in earn_titels:
+                if not token in tokens:
+                    tokens[token]= {
+                        trade_date.split()[0]: amount
+                    }
+                else:
+                    tokens[token][trade_date.split()[0]] = amount
+    return tokens
+
+def main():
+    client = Client(os.environ['B_API_KEY'],os.environ['B_API_SECRET'])
+    config = configparser.ConfigParser()
+    config.read(r'config.txt')
+    #klines = client.get_historical_klines("BTCUSDT", Client.KLINE_INTERVAL_1DAY, "1 Dec, 2017","2 Dec, 2017")
+    tokens = get_binance_transactions(config.get('binance', 'file'))
+    for k,v in tokens["BUSD"].items():
+        print(k,v)
+    #print(klines)
+
+if __name__ == "__main__":
+    main()
